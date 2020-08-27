9850ea6bcc9eeb7cd53fff9c7963fa6c9b8c7111
Nick <warebec@gmail.com>
2020-08-26 20:21:47 -0400

    still birthday function

--- bot.js
+++ bot.js
@@ -25,12 +25,12 @@ client.on('ready', () => {
       birthdayMember.roles.add(birthdayRole);
       client.channels.fetch('681914541029982268').then(channel => channel.send('<@' + birthdayMember + '> HAPPY BIRTHDAY!', {files: ["birthday.jpeg"]}));
     });
-    console.log(job.nextInvocation());
+    //console.log(job.nextInvocation());
     // removes role next day
     var revertJob = schedule.scheduleJob({month: info[1], date: parseInt(info[2]) + 1, hour: 0, minute: 0}, function(){
       birthdayMember.roles.remove(birthdayRole);
     });
-    console.log(revertJob.nextInvocation());
+    //console.log(revertJob.nextInvocation());
   });
 });
 
