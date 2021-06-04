43f6d1817f7ec6f99de3547f1aa4070fa302a10d
waregin <warebec@gmail.com>
2021-06-03 20:27:39 -0400

    how about now?

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const Discord = require('discord.js');
-const intents = new Discord.Intents([
+const { Client, Intents } = require('discord.js');
+const intents = new Intents([
     Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
     "GUILD_MEMBERS", // lets you request guild members (i.e. fixes the issue)
 ]);
-const client = new Discord.Client({ ws: { intents } });
+const client = new Client({ ws: { intents } });
 const cron = require("node-cron");
 const fs = require('fs');
 const lineReader = require('line-reader');
